041c6d1d178a541052f1dd4f0861310cc5a6ec47
Asheesh Dwivedi <asheesh.dwivedi@disneystreaming.com>
2022-12-01 13:23:30 -0500

    delete git ignore

---
+++ advent-2022/src/main/scala/day01/Day01.scala
@@ -0,0 +1,25 @@
+package day01
+
+import parser.TypeParser.*
+object Day01:
+
+  def sums(lines: Seq[String]): List[Long] = lines match
+    case Nil => Nil
+    case remaining =>
+      val (xs, n) = ksum(remaining)
+      n :: sums(xs)
+
+  def ksum(input: Seq[String]): (Seq[String], Long) = input match
+    case Nil      => (Nil, 0)
+    case "" :: xs => (xs, 0)
+    case n :: xs =>
+      val (rest, runningSum) = ksum(xs)
+      (rest, n.toInt + runningSum)
+
+  def part1(input: Seq[String]): Long = sums(input).max
+
+  def part2(input: Seq[String]): Long = sums(input).sorted.reverse.take(3).sum
+
+  @main def dayO1Part1: Unit = println(part1("day01/input1".as[Seq[String]]))
+
+  @main def dayO1Part2: Unit = println("day01/input2".as[Seq[String]])
---
+++ advent-2022/src/main/scala/scala.worksheet.sc
@@ -0,0 +1,6 @@
+import day01.Day01
+import parser.TypeParser._
+
+Day01.part1("day01/input1".as[Seq[String]])
+Day01.part2("day01/input2".as[Seq[String]])
+
