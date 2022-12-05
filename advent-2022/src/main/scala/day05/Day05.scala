94a9a8093f407aaaf7a775c56f085cf66f21f157
Asheesh Dwivedi <asheesh.dwivedi@disneystreaming.com>
2022-12-05 18:57:47 -0500

    Day 05

---
+++ advent-2022/src/main/scala/day05/Day05.scala
@@ -0,0 +1,48 @@
+package day05
+import parser.TypeParser
+import parser.TypeParser.*
+object Day05:
+
+  final case class Move(itemsToMove: Int, from: Int, to: Int)
+  object Move:
+    given TypeParser[String, Move] with
+      def parse(input: String): Move = input match
+        case s"move $n from $from to $to" => Move(n.toInt, from.toInt, to.toInt)
+
+  def parseStack(input: Seq[String]) = input
+    .takeWhile(_.nonEmpty)
+    .transpose
+    .filter(_.exists(_.isDigit))
+    .map(s => (s.last.toString.toInt -> s.dropRight(1).filter(_ != ' ')))
+    .toMap
+
+  def parseMove(input: Seq[String]) = input.dropWhile(_.nonEmpty).drop(1).map(_.as[Move])
+
+  def removeGroup(stack: Map[Int, Seq[Char]], moves: Seq[Move]): Map[Int, Seq[Char]] = moves match
+    case Nil                       => stack
+    case Move(qty, from, to) :: xs =>
+      val update = stack +
+        (to   -> (stack(from).take(qty) ++ stack(to))) +
+        (from -> stack(from).drop(qty))
+      removeGroup(update, xs)
+
+  // TODO improve we can do one by one we do not need reverse
+  def removeOneByOne(stack: Map[Int, Seq[Char]], moves: Seq[Move]): Map[Int, Seq[Char]] = moves match
+    case Nil                       => stack
+    case Move(qty, from, to) :: xs =>
+      val update = stack +
+        (to   -> (stack(from).take(qty).reverse ++ stack(to))) +
+        (from -> stack(from).drop(qty))
+      removeOneByOne(update, xs)
+
+  def part01(input: Seq[String]) = removeOneByOne(parseStack(input), parseMove(input))
+    .toSeq
+    .sortBy(_.head)
+    .map(_.last.head)
+    .mkString
+
+  def part02(input: Seq[String]) = removeGroup(parseStack(input), parseMove(input))
+    .toSeq
+    .sortBy(_.head)
+    .map(_.last.head)
+    .mkString
--- advent-2022/src/main/scala/scala.worksheet.sc
+++ advent-2022/src/main/scala/scala.worksheet.sc
@@ -1,10 +1,11 @@
+import parser.TypeParser.*
+import scala.collection.mutable.ArrayDeque
 import day01.Day01
 import day02.Day02
+import day02.Day02.*
 import day03.Day03
 import day04.Day04
-import day02.Day02.*
-import parser.TypeParser.*
-
+import day05.Day05
 Day01.part1("day01/input1".as[Seq[String]])
 Day01.part2("day01/input2".as[Seq[String]])
 Day02.part1("day02/input1".as[Seq[(RPS, RPS)]])
@@ -13,5 +14,8 @@ Day03.part1("day03/input1".as[Seq[String]])
 Day03.part2("day03/input1".as[Seq[String]])
 Day04.part1("day04/input1".as[Seq[String]])
 Day04.part2("day04/input1".as[Seq[String]])
+Day05.part01("day05/input1".as[Seq[String]])
+Day05.part02("day05/input1".as[Seq[String]])
+
 
 
