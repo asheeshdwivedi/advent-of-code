8531469d910605f351f1396736d5927abee0376d
Asheesh Dwivedi <asheesh.dwivedi@disneystreaming.com>
2022-12-06 09:42:12 -0500

    Day 06 rename

--- advent-2022/src/main/scala/day06/Day06.scala
+++ advent-2022/src/main/scala/day06/Day06.scala
@@ -2,12 +2,12 @@ package day06
 
 object Day06:
 
-  def solve(length: Int, input: String): Int =
-    def doSolve(soFar: String, remaining: String): Int =
+  def findMarker(length: Int, input: String): Int =
+    def findMarker(soFar: String, remaining: String): Int =
       if remaining.isEmpty() then 0
       else if remaining.take(length).toSet.size == length then soFar.length + length
-      else doSolve(soFar + remaining.head, remaining.tail)
-    doSolve(input.head.toString, input.tail)
+      else findMarker(soFar + remaining.head, remaining.tail)
+    findMarker(input.head.toString, input.tail)
 
-  def part1(input: Seq[String]) = solve(4, input.head)
-  def part2(input: Seq[String]) = solve(14, input.head)
+  def part1(input: Seq[String]) = findMarker(4, input.head)
+  def part2(input: Seq[String]) = findMarker(14, input.head)
